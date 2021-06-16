7e840011a8b177477ef6e504dfbd66aaad10cee1
david-bretaud <david.bretaud.pro@gmail.com>
2021-06-16 18:33:18 +0200

    Updates

---
+++ precache-manifest.25606554442e8e6ce84a590ccf29a5d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa9f729465c76edad5de9a188b76248",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "19120be201294a0e1888",
+    "url": "/blindtest-spotify/static/css/main.a84f3191.chunk.css"
+  },
+  {
+    "revision": "80d69267c094783cf89b",
+    "url": "/blindtest-spotify/static/js/2.aeb8e54d.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.aeb8e54d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19120be201294a0e1888",
+    "url": "/blindtest-spotify/static/js/main.920ee6dd.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);
